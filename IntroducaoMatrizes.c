88c80475fd26bd637f57a7e55e25b468bd179cf2
Danielprogrammer01 <dannielsantoscorreia@gmail.com>
2023-11-18 02:57:28 -0300

    concluido

--- IntroducaoMatrizes.c
+++ IntroducaoMatrizes.c
@@ -22,14 +22,17 @@ int main()
         }
         
     }
+
+    printf("\n");
        
     for (i = 0; i < 2; i++)
     {
         for (j = 0; j < 2; j++)
         {
-            printf("%d\n", numeros [i][j]);
+            printf("%d ", numeros [i][j]);
         }
-        
+
+        printf("\n");
     }
     
     return 0;
